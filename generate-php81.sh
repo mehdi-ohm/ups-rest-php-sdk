31617bfffd901384c02559f41f8556f3aa90bd94
Mehdi MAYER <mayer.mehdi@gmail.com>
2024-05-24 15:39:50 +0200

    Add specific generate script for old macos with php7.4 included by force

---
+++ generate-php81.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# Merge the individual OpenAPI definitions into a single file
+if [ -e openapi.yaml ]; then rm openapi.yaml; fi
+docker run -it --rm -v "$PWD":/app ghcr.io/elidrissidev/openapi-merge:1.5.2 \
+    --resolve-references 0 \
+    -o openapi.yaml \
+    openapi/Base.yaml \
+    openapi/*.yaml
+
+# Generate API classes
+/usr/local/opt/php@8.1/bin/php -d memory_limit=512M vendor/bin/jane-openapi generate -c .jane-openapi.php
